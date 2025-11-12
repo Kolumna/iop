17d71f2229feff8ef936ee69efd35ea18bd13f58
Michał Janiec <michaljaniec12@wp.pl>
2025-11-12 16:34:54 +0100

    added function fibonaci

--- program.cpp
+++ program.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
+int fibonaci(int n)
+{
+    if (n <= 1)
+        return n;
+    return fibonaci(n - 1) + fibonaci(n - 2);
+}
+
 int main()
 {
     int x;
     std::cin >> x;
     std::cout << x << std::endl;
+    std::cout << fibonaci(x) << std::endl;
     return 0;
 }
