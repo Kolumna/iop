b8135b5593e52442d56c79978d8ad832eb26ada0
Michał Janiec <michaljaniec12@wp.pl>
2025-11-12 16:49:37 +0100

    added input validator

--- program.cpp
+++ program.cpp
@@ -4,6 +4,13 @@ int main()
 {
     int x;
     std::cin >> x;
+
+    if (x < 0)
+    {
+        std::cout << "nie ma być wypisywana żadna wartość liczbowa" << std::endl;
+        return 0;
+    }
     std::cout << x << std::endl;
+
     return 0;
 }
